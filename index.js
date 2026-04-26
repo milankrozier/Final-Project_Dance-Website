f2647e159d8501821654377cbb2ac63ac3eb4f9f
Milan Rozier <94202748+milankrozier@users.noreply.github.com>
2026-04-26 00:14:59 +0000

    Edit 1.0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Add JavaScript below
+ScrollReveal().reveal('.homecoverpage' , 
+    {distance: '50 px' , 
+    origin: 'top' , 
+    duration: 2200});
+
+ScrollReveal().reveal('.featured-products' , 
+    {distance: '50 px' , 
+    origin: 'top' , 
+    duration: 3200});
